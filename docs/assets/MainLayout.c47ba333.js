f2f5be902dc88cc45e65b1766ecb92b830e8f086
imvenx <imvenx@proton.me>
2023-09-19 11:15:27 +0200

    add build

---
+++ docs/assets/MainLayout.c47ba333.js
@@ -0,0 +1,1 @@
+import{_ as o,r,o as t,c}from"./index.dac478f7.js";const n={};function a(_,s){const e=r("router-view");return t(),c(e)}var i=o(n,[["render",a]]);export{i as default};
--- quasar.config.js
+++ quasar.config.js
@@ -67,7 +67,7 @@ module.exports = configure(function (/* ctx */) {
       // ignorePublicFolder: true,
       // minify: false,
       // polyfillModulePreload: true,
-      // distDir
+      distDir: 'docs',
 
       // extendViteConf (viteConf) {},
       // viteVuePluginOptions: {},
