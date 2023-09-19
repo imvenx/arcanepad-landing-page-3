e229bec893ea089051c6859958361dc1c6f450cc
imvenx <imvenx@proton.me>
2023-09-19 18:17:11 +0200

    remove blank target for kickstarter link

--- src/components/MenuComp.vue
+++ src/components/MenuComp.vue
@@ -1,10 +1,9 @@
 <template>
   <div id="menu">
-    <!-- <a href="#">Kickstarter</a> -->
     <a href="https://discord.com/invite/6Pr9JBCGXy" target="_blank">Discord</a>
     <a href="https://www.youtube.com/channel/UCSmE-GM8iXoJRjraQYYycww" target="_blank">Videos</a>
     <a href="https://twitter.com/arcanepad" target="_blank">Twitter</a>
-    <a href="#" target="_blank" disabled title="soon!">Kickstarter</a>
+    <a href="#" disabled title="soon!">Kickstarter</a>
     <a href="#" disabled title="soon!">Develop</a>
     <a href="#" disabled title="soon!">Download</a>
     <a href="mailto:arcanepad@gmail.com" target="_blank">
